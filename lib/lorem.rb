5f9278761cc690c8403d15e554dafd0a2a488156
Amy Dusek <amlydu@umich.edu>
2015-12-19 17:55:21 -0500

    Create simple local gem that generates hipster lorem sentences

---
+++ lib/lorem.rb
@@ -0,0 +1,12 @@
+require "lorem/version"
+
+module Lorem
+  def self.ipsum
+    ["Fingerstache tofu paleo, 3 wolf moon fashion axe bushwick mixtape.",
+     "Affogato XOXO scenester, try-hard marfa cronut poutine master cleanse kogi cold-pressed etsy.",
+     "8-bit godard truffaut master cleanse, ennui skateboard ugh kinfolk.",
+     "Fanny pack YOLO pour-over polaroid readymade.",
+     "Narwhal brooklyn affogato, vinyl banh mi green juice next level.",
+     "Yuccie church-key letterpress, iPhone knausgaard austin 90's selvage raw denim. Pabst 90's DIY selfies."].sample
+  end
+end
